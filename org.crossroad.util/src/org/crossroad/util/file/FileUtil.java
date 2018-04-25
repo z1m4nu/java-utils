bf0cad957e6d358a0e2dd458920e3a90ab8d532b
e.soden <e.soden@gmail.com>
2018-04-25 10:20:02 +0200

    Add zip functions

--- org.crossroad.util/src/org/crossroad/util/file/FileUtil.java
+++ org.crossroad.util/src/org/crossroad/util/file/FileUtil.java
@@ -6,8 +6,16 @@ package org.crossroad.util.file;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.Properties;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipException;
+import java.util.zip.ZipInputStream;
+
+import org.apache.commons.io.IOUtils;
 
 /**
  * @author e.soden
@@ -16,10 +24,66 @@ import java.util.Properties;
 public final class FileUtil {
 
 	/**
+	 * Wraps the input stream with GZIPInputStream if needed.
 	 * 
+	 * @param inputStream
+	 * @return
+	 * @throws IOException
+	 * @throws ZipConvertionException
 	 */
-	private FileUtil() {
-		// TODO Auto-generated constructor stub
+	public static InputStream convertToZip(InputStream inputStream) throws IOException, ZipConvertionException {
+		inputStream = new ZipInputStream(inputStream);
+
+		if (((ZipInputStream) inputStream).getNextEntry() == null) {
+			throw new ZipConvertionException("Stream is not a zip file.");
+		}
+
+		return inputStream;
+	}
+
+	/**
+	 * 
+	 * @param inputStream
+	 * @param output
+	 * @return
+	 * @throws IOException
+	 */
+	public boolean unzip(InputStream inputStream, String output) throws IOException {
+
+		ZipInputStream zis = new ZipInputStream(inputStream);
+		ZipEntry entry;
+		boolean isEmpty = true;
+		File outputFolder = null;
+
+		try {
+
+			outputFolder = new File(output);
+			if (outputFolder.exists() && outputFolder.isDirectory() && outputFolder.canWrite()) {
+				zis = new ZipInputStream(inputStream);
+
+				while ((entry = zis.getNextEntry()) != null) {
+					isEmpty = false;
+					File newFile = new File(outputFolder, entry.getName());
+					if (newFile.getParentFile().mkdirs() && !entry.isDirectory()) {
+						FileOutputStream fos = new FileOutputStream(newFile);
+						IOUtils.copy(zis, fos);
+						fos.close();
+					}
+				}
+			} else {
+				throw new ZipException(output + " must point to a directory and have write access");
+			}
+
+		} finally {
+			if (zis != null) {
+				zis.close();
+			}
+			outputFolder = null;
+			entry = null;
+			zis = null;
+		}
+
+		return !isEmpty;
 	}
 
 	/**
@@ -48,6 +112,12 @@ public final class FileUtil {
 		return buffer.toString();
 	}
 
+	/**
+	 * 
+	 * @param file
+	 * @return
+	 * @throws Exception
+	 */
 	public static Properties loadProperties(String file) throws Exception {
 		Properties properties = null;
 		File f = new File(file);
