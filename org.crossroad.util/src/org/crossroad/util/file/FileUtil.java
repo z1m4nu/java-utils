359fd525b15142b8fb8ca753e8ec5bef9877ec07
e.soden <e.soden@gmail.com>
2018-04-25 10:35:35 +0200

    Add support in FileUtil for Zip file

--- org.crossroad.util/src/org/crossroad/util/file/FileUtil.java
+++ org.crossroad.util/src/org/crossroad/util/file/FileUtil.java
@@ -10,6 +10,7 @@ import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.Properties;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipException;
@@ -48,7 +49,7 @@ public final class FileUtil {
 	 * @return
 	 * @throws IOException
 	 */
-	public boolean unzip(InputStream inputStream, String output) throws IOException {
+	public static boolean unzip(InputStream inputStream, String output) throws IOException {
 
 		ZipInputStream zis = new ZipInputStream(inputStream);
 		ZipEntry entry;
@@ -127,6 +128,52 @@ public final class FileUtil {
 			properties.load(new FileInputStream(f));
 		}
 		return properties;
+	}
+
+	/**
+	 * Utility method to save InputStream data to target location/file
+	 * 
+	 * @param inStream
+	 *            - InputStream to be saved
+	 * @param target
+	 *            - full path to destination file
+	 */
+	public static void saveToFile(InputStream inStream, String target) throws IOException {
+		OutputStream out = null;
+		int read = 0;
+		byte[] bytes = new byte[1024];
+		try {
+			out = new FileOutputStream(new File(target));
+			while ((read = inStream.read(bytes)) != -1) {
+				out.write(bytes, 0, read);
+			}
+		} finally {
+
+			if (out != null) {
+				out.flush();
+				out.close();
+			}
+		}
+	}
+
+	/**
+	 * Creates a folder to desired location if it not already exists
+	 * 
+	 * @param dirName
+	 *            - full path to the folder
+	 * @throws SecurityException
+	 *             - in case you don't have permission to create the folder
+	 */
+	public static void createFolder(String dirName) throws SecurityException {
+		File theDir = null;
+		try {
+			theDir = new File(dirName);
+			if (!theDir.exists()) {
+				theDir.mkdir();
+			}
+		} finally {
+			theDir = null;
+		}
 	}
 
 }
